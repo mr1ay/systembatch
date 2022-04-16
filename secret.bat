fde34a045bfbbfdafb06b2b6b24b8b62dcf7e8a2
mr1ay <48817265+mr1ay@users.noreply.github.com>
2022-04-16 12:54:59 +0300

    hidden show file

--- secret.bat
+++ secret.bat
@@ -1,3 +1,6 @@
 @echo off
-if "%1"=="q"    attrib +h %2
-if "%1"=="qq"   attrib -h %2
+if "%1"=="q"        attrib +h %2
+if "%1"=="show"     attrib +h %2
+if "%1"=="hidden"   attrib -h %2
+if "%1"=="qq"       attrib -h %2
+
