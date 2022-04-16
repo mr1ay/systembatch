9ad2e6d1d5989eceabfb45b8ab134097a7f91d9b
mr1ay <48817265+mr1ay@users.noreply.github.com>
2022-04-16 13:02:21 +0300

    chrome -incognito

--- chrom.bat
+++ chrom.bat
@@ -1,6 +1,12 @@
 @echo off 
+set a=%*
+shift /1
+echo %a:~1%
 
-start chrome.exe -incognito --new-window  %*
+if "%a%"=="c"   start chrome.exe -incognito  %2 %3 
+if "%a%"=="cc"  start chrome.exe -incognito --new-window  %*
+
+
 timeout /t 1
 if "%1"=="e" exit
 
